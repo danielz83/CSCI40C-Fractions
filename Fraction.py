67290d116791fc667d390a1554164ba2bdbeb5a8
Christian Naguio <110177022+loltreeman@users.noreply.github.com>
2025-01-30 21:35:40 +0800

    Added conditions for the numerator and denominator in _init_ method

--- Fraction.py
+++ Fraction.py
@@ -13,7 +13,30 @@ class Fraction(object):
                 except ValueError:
                     self.numerator = 0
                     self.denominator = 1
-            return
+                    return
+
+                # This is to convert string to integers
+                numerator_str = numerator_str.strip()
+                denominator_str = denominator_str.strip()
+                
+                try:
+                    self.numerator = int(numerator_str)
+                    self.denominator = int(denominator_str)
+                # If it is not a number, the fraction will be set to 0/1.
+                except ValueError:
+                    self.numerator = 0
+                    self.denominator = 1
+                    return
+            # If the input string does not contain /, it will be treated as 
+            # whole number and denominator will be set to 1 as a result.
+            else:
+                try:
+                    self.numerator = int(stringed_fraction)
+                    self.denominator = 1
+                except ValueError:
+                    self.numerator = 0
+                    self.denominator = 1
+                    return
                 
     def gcd(a, b):
         #TODO
