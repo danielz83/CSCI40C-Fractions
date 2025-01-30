084eac8235948661a36df37c6ca91bc70c39c760
trix <naguio.greg@gmail.com>
2025-01-30 12:16:10 +0800

    Created and worked on statement on __init__ method

--- Fraction.py
+++ Fraction.py
@@ -1,11 +1,12 @@
 class Fraction(object):
-    # you want to get the number, input one viarbale and input another variable
-    
+    # if denominator is none, then you want to assume that the denominator is 1
     def __init__(self, numerator=0, denominator=1):
-        #TODO
+        #TODO: denominator is none, then you want to assume that denominator is 1
 
-        #
-        pass
+        if denominator is None:
+            if isinstance(numerator, int):
+                self.numerator = numerator
+                self.denominator = denominator
 
     def gcd(a, b):
         #TODO
